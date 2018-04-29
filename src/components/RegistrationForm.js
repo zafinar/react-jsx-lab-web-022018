949e35508f806d0d70ee87289959972dec23b1f6
zafinar <zafinar@gmail.com>
2018-04-29 19:37:18 -0400

    Done.

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class RegistrationForm extends React.Component{
+render(){
+  return(
+    <form>
+    <input type="text"/>
+    <input type="password"/>
+    <button type="submit" >submit</button> 
+    </form>
+
+
+  )
+}
+}
+
+export default RegistrationForm;
---
+++ src/components/Webpage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import FillerText from './FillerText.js'
+
+class Webpage extends React.Component{
+  render(){
+    return(
+      <div>
+      <title>"The world's coolest webpage"</title>
+      <FillerText />
+      <FillerText />
+      </div>
+    )
+  }
+}
+export default Webpage;
